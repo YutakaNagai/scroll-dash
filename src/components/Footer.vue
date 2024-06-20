f3c4d05bfcf979d04a9f11f28513c5dfeeba66e4
YutakaNagai <ynagai103@gmail.com>
2024-06-20 15:28:44 +0900

    基本素材配置

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+  <div class="footer">
+    <a href="https://craftwiz.jp/" target="_blank">
+      <img height="50" src="../assets/バナー_透過.png" />
+    </a>
+
+    <a href="https://github.com/YutakaNagai/scroll-dash" target="_blank">
+      <img width="50" src="../assets/github-mark.svg" />
+    </a>
+  </div>
+</template>
+
+<style scoped>
+.footer {
+  display: flex;
+  width: 100%;
+  justify-content: space-evenly;
+}
+</style>
